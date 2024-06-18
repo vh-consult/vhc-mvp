637fc2be94666700a265c45aa6bedaad05b6cddd
1Omon <1serwise@gmail.com>
2024-06-18 17:04:58 +0000

    made it a use client component

--- app/(root)/blogs/create-post/page.tsx
+++ app/(root)/blogs/create-post/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Loader from '@/components/Loader'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
