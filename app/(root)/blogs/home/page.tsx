97686ca42f16abe655863a3330b2b050cb409deb
1Omon <1serwise@gmail.com>
2024-06-20 09:46:01 +0000

    updated blog cards on blog page

--- app/(root)/blogs/home/page.tsx
+++ app/(root)/blogs/home/page.tsx
@@ -1,98 +1,53 @@
-import BlogCard from '@/components/landingpage/BlogCard'
+import CustomBlogCard from '@/components/blog/CustomBlogCard'
 import React from 'react'
 
 const BlogsHomePage = () => {
   return (
-    <div className='w-full grid grid-cols-4  '>
-      <BlogCard
-        className='bg-dark-1 md:w-[285px] md:h-[380px] rounded-none border-none my-2'
-        buttonText='Continue reading'
-        content='In recent times, the wide spread of HIV/AIDS among Ghanaian youth is 
-        appauling. With new trends in technology and health care service infrastructures....'
-        imageSrc='/images/doc-1.jpg'
-        title='Combating HIV/AIDS'
-        type='card'
-        author='NY Ayisi'
-        date='12/06/24'
-      />
-            <BlogCard
-        className='bg-dark-1 md:w-[285px] md:h-[380px] rounded-none border-none my-2'
-        buttonText='Continue reading'
-        content='In recent times, the wide spread of HIV/AIDS among Ghanaian youth is 
-        appauling. With new trends in technology and health care service infrastructures....'
-        imageSrc='/images/doc-1.jpg'
-        title='Combating HIV/AIDS'
-        type='card'
-        author='NY Ayisi'
-        date='12/06/24'
-      />
-            <BlogCard
-        className='bg-dark-1 md:w-[285px] md:h-[380px] rounded-none border-none my-2'
-        buttonText='Continue reading'
-        content='In recent times, the wide spread of HIV/AIDS among Ghanaian youth is 
-        appauling. With new trends in technology and health care service infrastructures....'
-        imageSrc='/images/doc-1.jpg'
-        title='Combating HIV/AIDS'
-        type='card'
-        author='NY Ayisi'
-        date='12/06/24'
-      />
-            <BlogCard
-        className='bg-dark-1 md:w-[285px] md:h-[380px] rounded-none border-none my-2'
-        buttonText='Continue reading'
-        content='In recent times, the wide spread of HIV/AIDS among Ghanaian youth is 
-        appauling. With new trends in technology and health care service infrastructures....'
-        imageSrc='/images/doc-1.jpg'
-        title='Combating HIV/AIDS'
-        type='card'
-        author='NY Ayisi'
-        date='12/06/24'
-      />
-            <BlogCard
-        className='bg-dark-1 md:w-[285px] md:h-[380px] rounded-none border-none my-2'
-        buttonText='Continue reading'
-        content='In recent times, the wide spread of HIV/AIDS among Ghanaian youth is 
-        appauling. With new trends in technology and health care service infrastructures....'
-        imageSrc='/images/doc-1.jpg'
-        title='Combating HIV/AIDS'
-        type='card'
-        author='NY Ayisi'
-        date='12/06/24'
-      />
-            <BlogCard
-        className='bg-dark-1 md:w-[285px] md:h-[380px] rounded-none border-none my-2'
-        buttonText='Continue reading'
-        content='In recent times, the wide spread of HIV/AIDS among Ghanaian youth is 
-        appauling. With new trends in technology and health care service infrastructures....'
-        imageSrc='/images/doc-1.jpg'
-        title='Combating HIV/AIDS'
-        type='card'
-        author='NY Ayisi'
-        date='12/06/24'
-      />
-            <BlogCard
-        className='bg-dark-1 md:w-[285px] md:h-[380px] rounded-none border-none my-2'
-        buttonText='Continue reading'
-        content='In recent times, the wide spread of HIV/AIDS among Ghanaian youth is 
-        appauling. With new trends in technology and health care service infrastructures....'
-        imageSrc='/images/doc-1.jpg'
-        title='Combating HIV/AIDS'
-        type='card'
-        author='NY Ayisi'
-        date='12/06/24'
-      />
-            <BlogCard
-        className='bg-dark-1 md:w-[285px] md:h-[380px] rounded-none border-none my-2'
-        buttonText='Continue reading'
-        content='In recent times, the wide spread of HIV/AIDS among Ghanaian youth is 
-        appauling. With new trends in technology and health care service infrastructures....'
-        imageSrc='/images/doc-1.jpg'
-        title='Combating HIV/AIDS'
-        type='card'
-        author='NY Ayisi'
-        date='12/06/24'
-      />
-    </div>
+    <section className='w-full flex flex-col pt-6'>
+      <div className="w-full pb-4">
+        <h1 className="text-3xl w-[60%] mx-auto text-center capitalize font-semibold">
+          Explore A Vast Library of health related articles and information that will greatly benefit you
+        </h1>
+        <p className="text-base opacity-75 w-[75%] text-center my-2 mx-auto">
+          We have gathered the most helpful tips, expert knowledge and guidance you may need to live a healthy life. With over 1000+ contributors, we can boast of a robust network of people who really care about helping you live a healthy life.
+        </p>
+        <div className="w-full">
+          <span className="text-lg font-medium"></span>
+          Recent Blogs
+        </div>
+      </div>
+      <div className="w-full grid grid-cols-4 ">
+        <CustomBlogCard
+          snippet='In recent times, the wide spread of the virus which had'
+          coverImage='/images/doc-1.jpg'
+          title='Combating HIV/AIDS With Advances In Technology'
+          author='NY Ayisi'
+          datePublished={new Date(2024, 5, 23)}
+        />
+        <CustomBlogCard
+          snippet='In recent times, the wide spread of the virus which had'
+          coverImage='/images/doc-1.jpg'
+          title='Combating HIV/AIDS With Advances In Technology'
+          author='NY Ayisi'
+          datePublished={new Date(2024, 5, 23)}
+        />
+        <CustomBlogCard
+          snippet='In recent times, the wide spread of the virus which had'
+          coverImage='/images/doc-1.jpg'
+          title='Combating HIV/AIDS With Advances In Technology'
+          author='NY Ayisi'
+          datePublished={new Date(2024, 5, 23)}
+        />
+        <CustomBlogCard
+          snippet='In recent times, the wide spread of the virus which had'
+          coverImage='/images/doc-1.jpg'
+          title='Combating HIV/AIDS With Advances In Technology'
+          author='NY Ayisi'
+          datePublished={new Date(2024, 5, 23)}
+        />
+      </div>
+      
+    </section>
   )
 }
 
---
+++ components/blog/CustomBlogCard.tsx
@@ -0,0 +1,44 @@
+import { ArrowUpRight } from 'lucide-react';
+import Image from 'next/image';
+import React from 'react'
+import { GoDot } from 'react-icons/go';
+
+interface CustomBlogProps {
+    author: string;
+    datePublished: Date;
+    title: string;
+    snippet: string;
+    coverImage: string;
+}
+
+const CustomBlogCard = ({author, datePublished, title, snippet, coverImage}: CustomBlogProps) => {
+  return (
+    <div className='bg-dark-1 md:w-[300px] md:h-[325px] rounded-none border-none my-2'>
+      <div className="w-full h-2/3">
+        <Image 
+            src={coverImage}
+            alt={`cover image`}
+            width={300}
+            height={150}
+            className='w-full h-full object-cover'
+        />
+      </div>
+      <div className="p-2">
+        <div className="flex items-center text-sm font-medium text-green-2">
+            <span className="">{author}</span>
+            <GoDot className='mx-1'/>
+            <span className="">{datePublished.toLocaleDateString()}</span>
+        </div>
+        <div className="w-full flex my-1">
+            <span className="text-base w-[95%] font-medium">{title}</span>
+            <ArrowUpRight className='w-5 h-5'/>
+        </div>
+        <span className="w-full text-sm">
+            {snippet.slice(0,42)}...
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default CustomBlogCard
