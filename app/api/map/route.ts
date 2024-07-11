80fbbf42c34594875b85076e80011876369a77e4
1Omon <1serwise@gmail.com>
2024-07-11 02:56:52 +0000

    commented out maps

--- app/api/map/route.ts
+++ app/api/map/route.ts
@@ -1,40 +1,40 @@
-//ts-nocheck
-import { handleError } from "@/lib/utils";
-import { OpenAI } from "openai"
+// //ts-nocheck
+// import { handleError } from "@/lib/utils";
+// import { OpenAI } from "openai"
 
-const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
+// const openai = new OpenAI();
 
-export async function POST(req: any, res:any) {
-    try {
-        const gpt4Completion = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo-0125",
-            messages: [
-                {
-                    role: 'system', 
-                    content: `You only return in JSON a 
-                    coordinate key with a value in this 
-                    format [43, 6426, -79.3871], then a 
-                    little of the location with a title key`
-                },
-                {
-                    role: 'user', content: req.body.value
-                }
-            ]
-        })
+// export async function POST(req: any, res:any) {
+//     try {
+//         const gpt4Completion = await openai.chat.completions.create({
+//             model: "gpt-3.5-turbo-0125",
+//             messages: [
+//                 {
+//                     role: 'system', 
+//                     content: `You only return in JSON a 
+//                     coordinate key with a value in this 
+//                     format [43, 6426, -79.3871], then a 
+//                     little of the location with a title key`
+//                 },
+//                 {
+//                     role: 'user', content: req.body.value
+//                 }
+//             ]
+//         })
 
-        const responseText =gpt4Completion.choices[0]?.message?.content;
-        if (responseText && responseText[0]==="{") {
-            const json = JSON.parse(responseText)
+//         const responseText =gpt4Completion.choices[0]?.message?.content;
+//         if (responseText && responseText[0]==="{") {
+//             const json = JSON.parse(responseText)
 
-            res.status(200).json(json)
+//             res.status(200).json(json)
 
-            console.log(json)
-        } else {
-            res.status(200).json({
-                tryAgain: true
-            })
-        }
-    } catch (error) {
-        handleError(error)
-    }
-}
+//             console.log(json)
+//         } else {
+//             res.status(200).json({
+//                 tryAgain: true
+//             })
+//         }
+//     } catch (error) {
+//         handleError(error)
+//     }
+// }
