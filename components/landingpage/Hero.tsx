943f73bc58ff35b6c0e97ee098adb93eb5bc731e
1Omon <1serwise@gmail.com>
2024-08-13 23:45:46 +0000

    still on hero page modification

--- components/landingpage/Hero.tsx
+++ components/landingpage/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <div className="w-full h-full relative"> 
         <div className="absolute inset-0 bg-green-4 bg-opacity-50"></div>
           
-          <div className=" w-3/5 h-1/2 absolute left-[20%] top-[25%] md:text-center text-white">
+          <div className=" w-3/5 h-1/2 absolute left-[20%] top-[22%] md:text-center text-white">
             <h5 className='text-sm md:text-2xl font-medium '>
               {landing.hero.subtitle}
             </h5>
@@ -21,19 +21,13 @@ const Hero = () => {
             <p className='font-medium text-lg'>
               {landing.hero.body}
             </p>
-            <SignedIn>
-              <Button className="h-[60px] px-8 mt-2 rounded-full bg-green-2 hover:opacity-85 text-xl">
-                <Link href={'/sign-up'}>
-                  Join Now!
-                </Link>
-              </Button>
-            </SignedIn>
+
           </div>
   
-          {/* <div className="
+          <div className="
             w-full card-section flex flex-row 
             items-center justify-center  flex-wrap text-green-4
-            absolute bottom-[-150px] md:bottom-[80px]  
+            absolute bottom-[5px]  
           ">
             {
               landing.hero.cards.map((card, index) =>
@@ -50,7 +44,7 @@ const Hero = () => {
                 }
               )
             }
-          </div> */}
+          </div>
   
         </div>
       </section>
